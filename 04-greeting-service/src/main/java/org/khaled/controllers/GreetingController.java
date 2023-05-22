8e32541eea0c6fcdc182e248721c91af166ae5d9
Khaled <khalegjr@gmail.com>
2023-05-22 15:35:48 -0300

    Refatora para utilizar variáveis de configuração

--- 04-greeting-service/src/main/java/org/khaled/controllers/GreetingController.java
+++ 04-greeting-service/src/main/java/org/khaled/controllers/GreetingController.java
@@ -2,7 +2,9 @@ package org.khaled.controllers;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.khaled.configuration.GreetingConfiguration;
 import org.khaled.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,16 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
+	private static final String template = "%s, %s!";
 	private final AtomicLong counter = new AtomicLong();
+	
+	@Autowired
+	private GreetingConfiguration configuration;
 	
 	@RequestMapping("/greeting")
 	public Greeting greeting(
 			@RequestParam(value="name",
-			defaultValue = "World") String name) {
+			defaultValue = "") String name) {
+		
+		if (name.isEmpty()) name = configuration.getDefaultValue();
+		
 		return new Greeting(
 					counter.incrementAndGet(),
-					String.format(template, name)
+					String.format(template, configuration.getGreeting(), name)
 				);
 	}
 }
